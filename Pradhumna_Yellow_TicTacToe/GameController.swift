da04faf79a27ada5693b58972c098229cd8a32ad
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-06-14 16:33:06 -0400

    added winning logic and behavious

--- Pradhumna_Yellow_TicTacToe/GameController.swift
+++ Pradhumna_Yellow_TicTacToe/GameController.swift
@@ -13,28 +13,36 @@ class GameController: UIViewController {
     @IBOutlet weak var messenger: UILabel!
     //initialize game model//
     var game = GameModel()
+    //variable to accept touch//
+    var acceptTouches = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     //action when box inside grid is pressed//
-    
     @IBAction func boxPressed(_ sender: UIButton) {
         print(sender.tag)
         print(game.movesPlayed)
+        print(game.gameResult)
         //to add player value in box//
-        if(sender.currentTitle == nil){
+        if(sender.currentTitle == nil && acceptTouches){
             //display currentPlayer value on box//
             sender.setTitle(game.currentPlayer, for: .normal)
             //toggle player//
             game.movePlayed(tag: sender.tag)
-            //display message
-            messenger.text = "\(game.currentPlayer)'s turn"
+            //check if game is over//
+            if(game.checkIfGameIsOver()) {
+                messenger.text = game.gameResult
+                //to stop accepting new touches//
+                acceptTouches = false
+            }else{
+                //displaying message for next turn//
+                messenger.text = "\(game.currentPlayer)'s turn"
+            }
+        }else{
+            
         }
-        
     }
     
-   
-
 }
--- Pradhumna_Yellow_TicTacToe/GameModel.swift
+++ Pradhumna_Yellow_TicTacToe/GameModel.swift
@@ -13,17 +13,71 @@ class GameModel {
     var currentPlayer = "X"
     //array to record moves//
     var movesPlayed = Array(repeating: " ", count: 9)
+    //for number of moves played//
+    var numberOfMovesPlayed = 0
+    //helper variable to check for winner//
+    var checkFor = "O"
+    //variable to store result//
+    var gameResult = ""
+    
+    
+    //winning combinations//
+    var winningPatterns = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,7]
+    ]
     
     //when user clickes on a box//
     func movePlayed(tag: Int){
+        //for number of moves//
+        numberOfMovesPlayed += 1
         //to store move into array//
         movesPlayed[tag-1] = currentPlayer
         
         //toggle current player//
         if(currentPlayer == "X") {
             currentPlayer = "O"
+            checkFor = "X"//toggle value to verify with winnig patterns//
         }else{
             currentPlayer = "X"
+            checkFor = "O"
         }
     }
+    
+    //function  for winning logic and if game is over//
+    func checkIfGameIsOver() -> Bool {
+        //a game needs to have atleast 5 moves for a player to win//
+        if(numberOfMovesPlayed < 5) {
+            return false
+        }
+        //for checking winning patterns for player//
+        for i in 0..<8 {
+            var winningPattern = winningPatterns[i]
+            
+            //verifying pateern and deciding gameResult//
+            if(movesPlayed[winningPattern[0] - 1] == checkFor &&
+               movesPlayed[winningPattern[1] - 1] == checkFor &&
+               movesPlayed[winningPattern[2] - 1] == checkFor) {
+                    //game is over//
+                    //checkFor has won//
+                    gameResult = checkFor + " Won!!!"
+                    //return true and game result
+                    return true
+            }
+        }
+        
+        //check for draw//
+        if(numberOfMovesPlayed == 9) {
+            gameResult = "It's a Draw"
+            return true
+        }
+        return false
+    }
+    
 }
