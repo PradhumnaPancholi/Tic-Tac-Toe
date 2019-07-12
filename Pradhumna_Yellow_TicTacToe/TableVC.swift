1176b7716eabc34abd1dbd47036237172b9e7955
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-07-12 16:58:52 -0400

    added functionality to display game history data in tableVC

--- Pradhumna_Yellow_TicTacToe/TableVC.swift
+++ Pradhumna_Yellow_TicTacToe/TableVC.swift
@@ -29,64 +29,31 @@ class TableVC: UITableViewController{
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        let numberOfGamesPlayed = UserDefaults.standard.integer(forKey: "numberOfGamesPlayed")
+        return numberOfGamesPlayed
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "pnp_tableCell", for: indexPath) as! PNP_TableCell
+        
+        //to retrieve data from userdefaults//
+        let numberOfGamesPlayed = UserDefaults.standard.integer(forKey: "numberOfGamesPlayed")
+        let thisCellIndex = numberOfGamesPlayed  - indexPath.row //to get data in reverse order//
+        let gameResult = UserDefaults.standard.string(forKey: "Result_" + String(thisCellIndex))
+        let timeStamp = UserDefaults.standard.double(forKey: "TimeStamp_" + String(thisCellIndex))
+        
 
-        // Configure the cell...
-        cell.datePlayed.text = "today"
-        cell.resultImg.image = UIImage(named: "yellow_win")
-        cell.whoWon.text = "Yellow Won"
+        // Configure the cell && and to display data accordingly//
+        cell.datePlayed.text = String(timeStamp)
+        cell.whoWon.text = gameResult
+        if( gameResult == "X Won!!!"){
+            cell.resultImg.image = UIImage(named: "yellow_win")
+        } else {
+            cell.resultImg.image = UIImage(named: "yellow_loss")
+        }
         
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
